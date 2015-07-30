7d1386d475119e18d1de2c8e0646dd8b4258682c
shauna1 <shauna1@naver.com>
2015-07-30 15:52:29 +0900

    Create ExOh.rb

---
+++ ExOh.rb
@@ -0,0 +1,15 @@
+def ExOh(str)
+
+  # code goes here
+  str = str.split("")
+  if str.count("x") == str.count("o")
+    return true
+  else
+    return false
+  end
+         
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+ExOh(STDIN.gets)           
